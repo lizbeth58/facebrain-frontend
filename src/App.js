75e00467160251a08b470806d8d75ef2a20ec7be
Elizabeth Ortiz <ortiz.beth58@gmail.com>
2020-03-04 16:42:18 -0800

    need to fix logo sizing

--- src/App.js
+++ src/App.js
@@ -5,16 +5,34 @@ import React from "react";
 import "./App.css";
 
 //* Components
+import Particles from "react-particles-js";
 import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 function App() {
   return (
     <div className='App'>
+      <Particles className='particles' params={particlesOptions} />
       <Navigation />
       <Logo />
-      {/*
+      <Rank />
       <ImageLinkForm />
+
+      {/*
       <FaceRecognition />
       */}
     </div>
